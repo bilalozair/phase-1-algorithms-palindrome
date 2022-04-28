31b8f79d591d443091c8c294a4cd0e0b85bbeace
bilalozair <bo569@nyu.edu>
2022-04-28 00:15:46 -0400

    commit tree 8cbe3b2a090a8d57bcb12fcd6d129719db96d015

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let reversedString = []
+    for (let i = 0; i <= word.length -1; i++) {
+     reversedString.unshift(word[i])
+    }
+
+  return word === reversedString.join('')
 }
+isPalindrome('racecar')
 
-/* 
-  Add your pseudocode here
-*/
+// /* 
+//   Add your pseudocode here
+// */
 
-/*
-  Add written explanation of your solution here
-*/
+// /*
+//   Add written explanation of your solution here
+// */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
